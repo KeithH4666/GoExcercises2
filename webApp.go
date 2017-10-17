7de38155909d5e6f918037afa2caa2b9be1c31d1
keith higgins <keithhigginsowns@gmail.com>
2017-10-17 18:19:10 +0100

    3rd exercise done

--- webApp.go
+++ webApp.go
@@ -1,18 +1,23 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+	"log"
 )
 
+/*
 func requestHandler(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type","text/html");// allows browser to render html tags
 
 	fmt.Fprintln(w, "<h1>Guessing Game</h1>")
 }
+*/
 
 func main() {
-	http.HandleFunc("/", requestHandler)
-	http.ListenAndServe(":8080", nil)
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/", fs)
+
+    log.Println("Listening...")
+    http.ListenAndServe(":8080", nil)
 }
