bd8017c6925f5f7ac7dd2165b8b8a8e089b853c3
keith higgins <keithhigginsowns@gmail.com>
2017-10-17 18:23:57 +0100

    minor edits

--- webApp.go
+++ webApp.go
@@ -18,6 +18,6 @@ func main() {
 	fs := http.FileServer(http.Dir("static"))
 	http.Handle("/", fs)
 
-    log.Println("Listening...")
+    log.Println("Preparing guessing game , enter this in your web browser - Localhost:8080")
     http.ListenAndServe(":8080", nil)
 }
