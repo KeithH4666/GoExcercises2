4b963f758bbbeb33bdb20d6cd29192b601d5f753
keith higgins <keithhigginsowns@gmail.com>
2017-10-26 16:50:58 +0100

    Added template to replace 'Have a guess'

--- webApp.go
+++ webApp.go
@@ -3,25 +3,32 @@ package main
 import (
 	"net/http"
 	"log"
+	"text/template"
+
 )
 
-/*
-func requestHandler(w http.ResponseWriter, r *http.Request) {
 
-	w.Header().Set("Content-Type","text/html");// allows browser to render html tags
+type guessStr struct{
 
-	fmt.Fprintln(w, "<h1>Guessing Game</h1>")
+	Guess string
+
 }
-*/
+
+func guess(w http.ResponseWriter, r *http.Request){
+
+	t, _ := template.ParseFiles("template/time.html")
+	t.Execute(w, guessStr{Guess: "Pick a number between 1-20:"})
+
+}
 
 func main() {
 	
-	
-	fs := http.FileServer(http.Dir("static"))
+	fs := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fs)
+	http.HandleFunc("/guess",guess)
+
 
     log.Println("Preparing guessing game , enter this in your web browser - Localhost:8080")
-    http.ListenAndServe(":8080", nil)
-	
+    http.ListenAndServe(":8080", nil)	
 	
 }
